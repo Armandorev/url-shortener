5f91a843bc9199809ea3c0565eaa11c5826fe640
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2015-06-29 15:06:25 +0100

    Reading WordNet database files

---
+++ src/main/java/benjamin/groehbiel/ch/words/WordNetHelper.java
@@ -0,0 +1,68 @@
+package benjamin.groehbiel.ch.words;
+
+import benjamin.groehbiel.ch.shortener.word.WordDefinition;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.nio.file.Files.lines;
+
+public class WordNetHelper {
+
+    public static List<WordDefinition> parse(String filename) throws IOException {
+
+        Stream<String> lines = lines(Paths.get(filename), Charset.defaultCharset());
+        List<WordDefinition> words = lines
+                .map(line -> {
+                    Pattern validLinePattern = Pattern.compile("[0-9]{8,8} [^ ]* [^ ]* [^ ]* ([a-z]*).*\\| (.*)$");
+                    Matcher matcher = validLinePattern.matcher(line);
+                    if (matcher.find()) {
+                        String word = matcher.group(1);
+                        String desc = matcher.group(2);
+                        return new WordDefinition(word, desc);
+                    } else {
+                        return null;
+                    }
+                })
+                .filter(w -> Objects.nonNull(w))
+                .collect(Collectors.toList());
+
+        return words;
+    }
+
+
+    public static List<Path> scan(String wordNetDirectoryPath) throws IOException {
+        File wordNetDirectory = new File(wordNetDirectoryPath);
+
+        ArrayList<Path> dictFiles = new ArrayList<>();
+        for (File f : wordNetDirectory.listFiles()) {
+            dictFiles.add(Paths.get(f.getPath()));
+        }
+
+        return dictFiles;
+    }
+
+
+    public static List<WordDefinition> load(String wordNetDirectory) throws IOException {
+
+        List<WordDefinition> allWords = new ArrayList<>();
+
+        List<Path> wordNetFiles = scan(wordNetDirectory);
+        for (Path dictFile : wordNetFiles) {
+            List<WordDefinition> wordsInDocument = parse(dictFile.toString());
+            allWords.addAll(wordsInDocument);
+        }
+
+        return allWords;
+    }
+}
---
+++ src/test/java/benjamin/groehbiel/ch/words/WordNetHelperTest.java
@@ -0,0 +1,40 @@
+package benjamin.groehbiel.ch.words;
+
+import benjamin.groehbiel.ch.shortener.word.WordDefinition;
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+
+
+public class WordNetHelperTest {
+
+    @Test
+    public void shouldParseWordNetFileAndReturnDictioary() throws Exception {
+        List<WordDefinition> dict = WordNetHelper.parse("src/test/resources/WordNet/test_data.adj");
+        assertThat(dict, hasSize(20));
+    }
+
+    @Test
+    public void shouldListAllWordNetFilesInResources() throws Exception {
+        List<Path> wordnetFiles = WordNetHelper.scan("src/test/resources/WordNet/");
+        assertThat(wordnetFiles, hasSize(2));
+        assertThat(wordnetFiles, contains(
+                Paths.get("src/test/resources/WordNet/test_data.adj"),
+                Paths.get("src/test/resources/WordNet/test_data2.adj")
+        ));
+    }
+
+    @Test
+    public void shouldReturnAListOfAllWordDefinitions() throws Exception {
+        List<WordDefinition> words = WordNetHelper.load("src/test/resources/WordNet/");
+        MatcherAssert.assertThat(words, hasSize(21));
+    }
+
+}
