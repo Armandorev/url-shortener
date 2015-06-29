9ba3ceb2248677a3fa15ffa036bf779ff7ccdd2c
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2015-06-29 17:08:54 +0100

    Taking explicit filter definition out, is registered automatically.

--- src/main/java/benjamin/groehbiel/ch/Application.java
+++ src/main/java/benjamin/groehbiel/ch/Application.java
@@ -4,9 +4,7 @@ import benjamin.groehbiel.ch.shortener.ShortenerRepository;
 import benjamin.groehbiel.ch.shortener.word.WordBasedRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
 
 @SpringBootApplication
 public class Application {
@@ -18,14 +16,6 @@ public class Application {
     @Bean
     public ShortenerRepository shortenerRepository() {
         return new WordBasedRepository();
-    }
-
-    @Bean
-    public FilterRegistrationBean redirectFilterRegister(RedirectFilter redirectFilter) {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(redirectFilter);
-        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return registration;
     }
 
 }
