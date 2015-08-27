9db56b06ba4600ad44b24c6927cffadc6afb1498
Benjamin Gröhbiel <benjamin.groehbiel@gmail.com>
2015-08-27 22:42:42 +0100

    Test: Add all wordnet entries to database

--- src/test/java/benjamin/groehbiel/ch/PostgresTest.java
+++ src/test/java/benjamin/groehbiel/ch/PostgresTest.java
@@ -1,18 +1,24 @@
 package benjamin.groehbiel.ch;
 
-import benjamin.groehbiel.ch.shortener.DictionaryHash;
-import benjamin.groehbiel.ch.shortener.DictionaryRepository;
+import benjamin.groehbiel.ch.shortener.*;
+import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.sql.DataSource;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -48,6 +54,20 @@ public class PostgresTest {
         DictionaryHash newDictionaryItem = dictionaryRepository.save(dictionaryItem);
         assertThat(dictionaryRepository.count(), equalTo(1L));
         assertThat(newDictionaryItem.getHashId(), equalTo(1L));
+    }
+
+    @Test
+    public void addAllHashesToDatabase() throws IOException {
+        File wordNet = new ClassPathResource("WordNet").getFile();
+        List<WordDefinition> englishWords = WordNetHelper.load(wordNet.getPath());
+
+        List<DictionaryHash> dictionaryHashes = englishWords.stream().map(w -> {
+            return new DictionaryHash(w.getWord(), "en", w.getDescription(), false);
+        }).collect(toList());
+
+        dictionaryRepository.save(dictionaryHashes);
+
+        assertThat(dictionaryRepository.count(), equalTo((long) 21));
     }
 
 }
