134485f6104a68556737e53d4498f85b6a80c73d
Benjamin Groehbiel <benjamin.groehbiel@gmail.com>
2016-02-15 15:52:30 +0000

    Tidy up ShortenerServiceTest

--- src/test/java/it/w0rd/api/ShortenerServiceTest.java
+++ src/test/java/it/w0rd/api/ShortenerServiceTest.java
@@ -21,30 +21,18 @@ public class ShortenerServiceTest extends DataTest {
     private ShortenerService shortenerService;
 
     @Test
-    public void shouldReturnARealWordAsHashInShortenedUrl() throws URISyntaxException, IOException {
-        URI inputUri = new URI("http://www.example.org");
-        ShortenerHandle shortenerHandle = shortenerService.shorten(inputUri);
+    public void shouldShortenUrl() throws URISyntaxException, IOException {
+        ShortenerHandle shortenerHandle = shortenUrl("http://www.example.org");
         assertThat(shortenerHandle.getHash(), equalTo("able"));
-
-        URI anotherShortenedUri = new URI("http://www.example2.org");
-        shortenerHandle = shortenerService.shorten(anotherShortenedUri);
-        assertThat(shortenerHandle.getHash(), equalTo("unable"));
     }
 
     @Test
-    public void shouldLookupUrlInRedisGivenAHash() throws URISyntaxException, IOException {
-        redisManager.setUrlAndHash("$count", "0");
-
-        assertThat(shortenerService.getShortenedCount(), equalTo(0L));
-
-        redisManager.setUrlAndHash("$count", "1");
-        ShortenerHandle shortenerHandleForWater = new ShortenerHandle();
-        String value = new ObjectMapper().writeValueAsString(shortenerHandleForWater);
-
+    public void shouldLookupHash() throws URISyntaxException, IOException {
+        String value = new ObjectMapper().writeValueAsString(new ShortenerHandle());
         redisManager.setUrlAndHash(RedisManager.HASH_PREFIX + "water", value);
 
         ShortenerHandle shortenerHandle = shortenerService.expand("water");
-        assertThat(shortenerHandle, equalTo(shortenerHandleForWater));
+        assertThat(shortenerHandle, equalTo(new ShortenerHandle()));
     }
 
     @Test
@@ -58,16 +46,20 @@ public class ShortenerServiceTest extends DataTest {
     }
 
     @Test
-    public void shouldRemoveExistingShortenedUrlAndMakeAvailable() throws IOException, URISyntaxException {
-        ShortenerHandle handle = shortenerService.shorten(URI.create("http://www.example.com"));
-        String hashToDelete = handle.getHash();
+    public void shouldRemoveExistingShortenedUrlAndFreeItUp() throws IOException, URISyntaxException {
+        ShortenerHandle shortenedUrl = shortenUrl("http://www.example.com");
 
         MatcherAssert.assertThat(dictionaryManager.getWordsAvailableSize(), IsEqual.equalTo(20L));
         assertThat(shortenerService.getShortenedCount(), IsEqual.equalTo(1L));
 
-        shortenerService.remove(hashToDelete);
+        shortenerService.remove(shortenedUrl.getHash());
 
         MatcherAssert.assertThat(dictionaryManager.getWordsAvailableSize(), IsEqual.equalTo(21L));
         assertThat(shortenerService.getShortenedCount(), IsEqual.equalTo(0L));
     }
+
+    private ShortenerHandle shortenUrl(String url) throws URISyntaxException, IOException {
+        return shortenerService.shorten(new URI(url));
+    }
+
 }
